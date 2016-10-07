a1f8dbe2ab0a2e53162b2a1889fa9e58d61a7b1c
Yuka Kato <satoimo.to.imo@gmail.com>
2016-10-07 13:47:23 +0900

    Parse title and ranking, and save to CSV file

--- amazon_ranking_crawlers/app/processors/ranking_processor.rb
+++ amazon_ranking_crawlers/app/processors/ranking_processor.rb
@@ -1,16 +1,27 @@
 require "daimon_skycrawlers/processor"
 require "pry"
 require "nokogiri"
+require "csv"
 
 class MyProcessor < DaimonSkycrawlers::Processor::Base
   def call(message)
     return if message[:heartbeat]
+
     url = message[:url]
-
     page = storage.find(url)
-
     doc = Nokogiri::HTML(page.body)
-    doc.xpath("//div[@class='zg_title']/a/text()").each{|text| pp text.content }
+
+    rankings = doc.xpath("//div[@class='zg_item_normal']").inject([]) do |results, element|
+      rank = element.xpath(".//span[@class='zg_rankNumber']/text()").text.gsub(/\./, "")
+      title = element.xpath(".//div[@class='zg_title']/a/text()").text
+      results << [rank, title]
+    end
+
+    CSV.open("amazon_ranking.csv", "w") do |csv|
+      rankings.each do |row|
+        csv << row
+      end
+    end
   end
 end
 
