b70bc03355a7d1ac712b31fbd90de4840a9acc87
Yuka Kato <satoimo.to.imo@gmail.com>
2016-10-17 19:20:52 +0900

    Imprement filter

--- re_enqueue_crawlers/app/filters/index_page_filter.rb
+++ re_enqueue_crawlers/app/filters/index_page_filter.rb
@@ -1,7 +1,9 @@
 require "daimon_skycrawlers/filter/base"
 
 class IndexPageFilter < DaimonSkycrawlers::Filter::Base
+  # XXX 他の checker 系（Filter::Base を継承したもの）との対称性を取るのであれば
+  #     index ページだったときには false を返すようにするべきなのかもしれないけど...
   def call(url)
-    puts "hogehogehogehoge"
+    /https:\/\/connpass.com\/search\//.match(url) ? true : false
   end
 end
