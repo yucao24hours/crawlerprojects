ed7daa7e4d270263531f83e1f20fec2ba412e549
Yuka Kato <satoimo.to.imo@gmail.com>
2016-10-21 18:00:18 +0900

    [WIP]Use Mechanize

--- hoge_crawler/app/crawlers/amazon_crawler.rb
+++ hoge_crawler/app/crawlers/amazon_crawler.rb
@@ -33,6 +33,21 @@ crawler = AmazonCrawler.new(base_url)
 crawler.prepare do |connection|
 
   # ログイン
+  agent = Mechanize.new
+
+  # XXX GET する URL はエンキューされたときに指定されるのでそれを使いたい
+  #     prepare の中でそれを参照するにはどうしたらいいのか？
+  agent.get("https://amazon.co.jp") do |page|
+    new_registration_page = agent.click(page.link_with(text: /新規登録はこちら/))
+    signin_page = agent.click(new_registration_page.link_with(text: /サインイン/))
+    my_page = signin_page.form_with(name: "signIn") do |f|
+      f.field_with(id: "ap_email").value = ENV["AMAZON_EMAIL"]
+      f.field_with(id: "ap_password").value = ENV["AMAZON_PASSWORD"]
+    end.submit
+    # ここで my_page には、明らかにログイン後じゃない画面が入ってくる。
+    # これはなんなのか？（Status Code は 200）
+    # そしてここからどうやってすすめばいいのか？
+    binding.pry
   end
 end
 
