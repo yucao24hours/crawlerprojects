912ded626e9f1b7b6a6973565dd479b8cdb4e3eb
Yuka Kato <satoimo.to.imo@gmail.com>
2016-10-21 18:00:03 +0900

    Remove Capybara codes

--- hoge_crawler/app/crawlers/amazon_crawler.rb
+++ hoge_crawler/app/crawlers/amazon_crawler.rb
@@ -1,11 +1,9 @@
 require "daimon_skycrawlers/crawler"
 require 'daimon_skycrawlers/filter/update_checker'
 require "daimon_skycrawlers/crawler/base"
-require "open-uri"
-require "capybara"
-require "capybara/dsl"
-require "launchy"
 require "dotenv"
+require "mechanize"
+require "open-uri"
 
 require 'pry'
 require "pry-nav"
@@ -13,8 +11,6 @@ require "pry-nav"
 Dotenv.load
 
 class AmazonCrawler < DaimonSkycrawlers::Crawler::Base
-  include Capybara::DSL
-
   def fetch(url, **kw)
     update_checker = DaimonSkycrawlers::Filter::UpdateChecker.new(storage: storage)
     unless update_checker.call(url.to_s, connection: connection)
@@ -35,26 +31,9 @@ base_url = "http://example.com"
 crawler = AmazonCrawler.new(base_url)
 
 crawler.prepare do |connection|
-  include Capybara::DSL
 
   # ログイン
-  Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
-  Capybara.current_driver = :selenium
-  Capybara.app_host = "https://amazon.co.jp"
-  Capybara.default_max_wait_time = 30
-
-  visit "/"
-
-  click_link "新規登録はこちら"
-
-  click_link "サインイン"
-
-  fill_in "ap_email", with: ENV["AMAZON_EMAIL"]
-  fill_in "ap_password", with: ENV["AMAZON_PASSWORD"]
-
-  click_on "signInSubmit"
 end
 
 DaimonSkycrawlers.register_crawler(crawler)
